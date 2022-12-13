f08d347f00ba6017b9554dbccf17353dfffa79be
bitbytejoy <bitbytejoy@gmail.com>
2022-12-13 11:54:36 +0100

    feat: Map example

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,12 @@ function App() {
     alert(`Es wurde ${name} geclickt`);
   }
 
+  const items = [
+    {id: "1", name: "John", age: 30},
+    {id: "2", name: "Tom", age: 20},
+    {id: "3", name: "Marry", age: 25},
+  ];
+
   return (
     <div>
       <Greeting {...greeting1Props} onClick={onClick}>Extra body</Greeting>
@@ -23,6 +29,25 @@ function App() {
       <Greeting name="Doreen" age={30} onClick={onClick}>
         <button>Oha</button>
       </Greeting>
+
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>name: {item.name}, age: {item.age}</li>
+        ))}
+      </ul>
+
+      <div>
+        {items.map((item, i) => (
+          <Greeting
+            key={item.id}
+            name={item.name}
+            age={item.age}
+            onClick={() => {}}
+          >
+            Index: {i}
+          </Greeting>
+        ))}
+      </div>
     </div>
   );
 }
